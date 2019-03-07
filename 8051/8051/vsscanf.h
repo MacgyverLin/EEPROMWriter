e3c9a7a8ca3d9e07a6ae69689c83b32ce72bb54b
Mac <macgyvercct@yahoo.com.hk>
2019-03-07 11:57:12 +0800

    1

--- 8051/8051/vsscanf.h
+++
@@ -1,9 +0,0 @@
-#ifndef _VSSCANF_H_
-#define _VSSCANF_H_
-
-#include <stdarg.h>
-
-extern int atob(long *vp, char *p, int base);
-extern int vsscanf(const char *buf, const char *s, va_list ap);
-
-#endif
